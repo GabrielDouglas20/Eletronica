988836f8dcaa7c40c1e644e3108abb2852a2122a
Edgar <edgaralmeida0110@gmail.com>
2025-09-19 19:49:01 -0300

    Alteração 1: designr

---
+++ FrmPesquisas.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace projeto_1
+{
+    partial class FrmPesquisas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnPesquisarUsuarioP = new System.Windows.Forms.Button();
+            this.dataGridViewUsuP = new System.Windows.Forms.DataGridView();
+            this.MaskUsuarioP = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsuP)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnPesquisarUsuarioP
+            // 
+            this.BtnPesquisarUsuarioP.Location = new System.Drawing.Point(12, 61);
+            this.BtnPesquisarUsuarioP.Name = "BtnPesquisarUsuarioP";
+            this.BtnPesquisarUsuarioP.Size = new System.Drawing.Size(75, 23);
+            this.BtnPesquisarUsuarioP.TabIndex = 0;
+            this.BtnPesquisarUsuarioP.Text = "Pesquisar";
+            this.BtnPesquisarUsuarioP.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewUsuP
+            // 
+            this.dataGridViewUsuP.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewUsuP.Location = new System.Drawing.Point(12, 90);
+            this.dataGridViewUsuP.Name = "dataGridViewUsuP";
+            this.dataGridViewUsuP.Size = new System.Drawing.Size(606, 150);
+            this.dataGridViewUsuP.TabIndex = 1;
+            // 
+            // MaskUsuarioP
+            // 
+            this.MaskUsuarioP.Location = new System.Drawing.Point(93, 64);
+            this.MaskUsuarioP.Name = "MaskUsuarioP";
+            this.MaskUsuarioP.Size = new System.Drawing.Size(100, 20);
+            this.MaskUsuarioP.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "USUARIO";
+            // 
+            // FrmPesquisas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MaskUsuarioP);
+            this.Controls.Add(this.dataGridViewUsuP);
+            this.Controls.Add(this.BtnPesquisarUsuarioP);
+            this.Name = "FrmPesquisas";
+            this.Text = "FrmPesquisas";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsuP)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnPesquisarUsuarioP;
+        private System.Windows.Forms.DataGridView dataGridViewUsuP;
+        private System.Windows.Forms.MaskedTextBox MaskUsuarioP;
+        private System.Windows.Forms.Label label1;
+    }
+}
